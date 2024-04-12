63b3906992556292193928bcf24257a02e6a8fce
Juan Salvador Fructuoso Campoy <p-jfructuosoc@neoris.com>
2024-04-12 08:45:30 +0200

    Creada la entidad de Intervinientes

--- msvc-intervinientes/src/main/java/com/juansa/msvcintervinientes/entities/Intervinientes.java
+++ msvc-intervinientes/src/main/java/com/juansa/msvcintervinientes/entities/Intervinientes.java
@@ -1,11 +1,39 @@
 package com.juansa.msvcintervinientes.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 
 @Entity
+@Data
+@NoArgsConstructor
 @Table(name = "intervinientes")
 public class Intervinientes {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "nombre")
+    @NotEmpty(message = "no puede quedar vacío")
+    private String nombre;
+
+    @Column(name = "tipo_intervencion")
+    @NotEmpty(message = "no puede quedar vacío")
+    private String tipoIntervencion;
+
+    @Column(name = "fecha_creacion")
+    private LocalDate fechaCreacion;
+
+    @Column(name = "usuario_creacion")
+    private String usuarioCreacion;
+
+    @Column(name = "fecha_modificacion")
+    private LocalDate fechaModificacion;
+
+    @Column(name = "usuario_modificacion")
+    private String usuarioModificacion;
+
 }
