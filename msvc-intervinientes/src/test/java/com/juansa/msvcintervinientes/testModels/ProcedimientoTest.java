0103dd97508cb1a4c1fd11c7050c97a229a7f842
Juan Salvador Fructuoso Campoy <juansacampoy.train@gmail.com>
2024-04-16 17:57:35 +0200

    97% de test coverage en intervinientes

--- msvc-intervinientes/src/test/java/com/juansa/msvcintervinientes/testModels/ProcedimientoTest.java
+++ msvc-intervinientes/src/test/java/com/juansa/msvcintervinientes/testModels/ProcedimientoTest.java
@@ -5,26 +5,39 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class ProcedimientoTest {
+class ProcedimientoTest {
 
     @Test
-    public void testGetAndSetId() {
+    void testGetAndSetId() {
         Procedimiento procedimiento = new Procedimiento();
         procedimiento.setId(1L);
         assertEquals(1L, procedimiento.getId());
     }
 
     @Test
-    public void testGetAndSetNumeroProcedimiento() {
+    void testGetAndSetNumeroProcedimiento() {
         Procedimiento procedimiento = new Procedimiento();
         procedimiento.setNumeroProcedimiento("1234");
         assertEquals("1234", procedimiento.getNumeroProcedimiento());
     }
 
     @Test
-    public void testGetAndSetAnno() {
+    void testGetAndSetAnno() {
         Procedimiento procedimiento = new Procedimiento();
         procedimiento.setAnno(2022);
         assertEquals(2022, procedimiento.getAnno());
+    }
+
+    @Test
+    void testAllArgsConstructor() {
+        Long id = 1L;
+        String numeroProcedimiento = "1234";
+        int anno = 2022;
+
+        Procedimiento procedimiento = new Procedimiento(id, numeroProcedimiento, anno);
+
+        assertEquals(id, procedimiento.getId());
+        assertEquals(numeroProcedimiento, procedimiento.getNumeroProcedimiento());
+        assertEquals(anno, procedimiento.getAnno());
     }
 }
